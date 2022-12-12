bf329a3bf3449e08916ce36852c580d4ec8c531e
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-12 12:03:24 -0300

    .

---
+++ exercicios/ex03.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt('Digite o primeiro numero:'))
+var n2 = Number(window.prompt('Digite o segundo numero:'))
+var soma = n1 + n2
+window.alert(`A soma dos valores é: ${soma}`)
---
+++ exercicios/ex04.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome?')
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras`)
+document.write(`Seu nome em maiusculas é ${nome.toUpperCase()}`)
+document.write(`Seu nome em minusculas é ${nome.toLowerCase()}`)
