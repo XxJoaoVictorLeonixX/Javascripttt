dc1c70dc579f68b6cf34fa4f0932add1d88ab5d1
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-13 00:17:11 -0300

    .

---
+++ exercicios/ex07.js
@@ -0,0 +1,10 @@
+
+function somar(){
+    var tn1 = document.querySelector('#num1')
+    var tn2 = document.querySelector('#num2')
+    var res = document.querySelector('#res')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var s = n1 + n2
+    res.innerHTML = `A soma entre ${n1} + ${n2} é igual a ${s}`
+}
