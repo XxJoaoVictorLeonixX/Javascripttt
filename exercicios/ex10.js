1b69e7a2ab0ca7891cef3fe1185dc1a266ce7b77
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-14 12:39:50 -0300

    .

--- exercicios/ex10.js
+++ exercicios/ex10.js
@@ -1,18 +1,23 @@
 function carregar(){
     var msg = document.querySelector('#msg')
     var img = document.querySelector('#manha')
+    var dia = document.querySelector('#dia')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    
 
     if(hora >=0 && hora < 12){
         img.src = '../imagens/manha.jpg'
         document.body.style.backgroundColor ='orange'
+        dia.innerHTML = 'Bom dia'
     }else if(hora >= 12 && hora < 18){
         img.src = '../imagens/tarde.jpg'
         document.body.style.backgroundColor ='#698FF5'
+        dia.innerHTML = 'Boa tarde'
     }else{
         img.src = '../imagens/noite.jpg'
         document.body.style.backgroundColor ='#C357D4'
+        dia.innerHTML = 'Boa noite'
     }
 }
