0dc587acb595002c02189f4eb9bbe7b26c16060a
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-16 11:59:32 -0300

    .

---
+++ exercicios/array02.js
@@ -0,0 +1,6 @@
+let num = [8,1,7,4,2,9]
+num.sort()
+
+for(let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem os valor ${num[pos]}`)
+}
---
+++ exercicios/array03.js
@@ -0,0 +1,7 @@
+let num = [5,8,6,3,2,4]
+num.sort()
+
+//So funciona para Array e objetos
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
