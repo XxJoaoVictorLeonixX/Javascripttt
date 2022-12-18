2adee55e93164e7c1fa97aab4aee7fc5221e9bc5
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-17 21:13:45 -0300

    Update array.js

--- exercicios/array.js
+++ exercicios/array.js
@@ -8,5 +8,5 @@ let pos = num.indexOf(2)
 if(pos == -1){
     console.log('O valor não foi encontrado!')
 }else{
-    console.log(`O valor 8 esta na posição ${pos}`)
+    console.log(`O valor 2 esta na posição ${pos}`)
 }
