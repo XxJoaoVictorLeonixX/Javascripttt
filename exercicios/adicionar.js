eab90b8d1556c173dd226771a7d26860d188e3f0
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-20 13:00:13 -0300

    .

--- exercicios/adicionar.js
+++ exercicios/adicionar.js
@@ -20,8 +20,34 @@ function inLista(n,l) {
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        alert('Tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else{
         alert('Valor invalido ou ja encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores){
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
     }
 }
